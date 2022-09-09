023084393923a21d2fe301e3982eb2bbf2dddb2d
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-09-09 07:54:31 -0700

    Write a C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include <stdio.h>
+/**
+ * main - printing to standard output using "puts"
+ *
+ * Return - 0 (Success)
+ */
+
+int main(0)
+
+puts("Programming is like building a multilingual puzzle");
+
+return (0);
