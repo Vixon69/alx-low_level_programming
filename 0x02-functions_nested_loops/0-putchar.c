2b89b448dd9e76d0519feb87a276a3cef4dae780
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-09-13 07:22:32 -0700

    Write a program that prints _putchar, followed by a new line.
    The program should return 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Print _putchar
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (success)
  */
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	char text[9];
+	text = '_putchar';
+
+	_putchar(char text);
+
+	return (0);
 }
