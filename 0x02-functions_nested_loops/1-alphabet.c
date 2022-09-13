a72dd7c04449c6b8db57b4901620e188df1ad0d5
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-09-13 08:13:18 -0700

    Write a function that prints the alphabet, in lowercase, followed by a new line.
    Prototype: void print_alphabet(void);
    You can only use _putchar twice in your code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - write a function that prints the alphabet
- *
+ * print_alphabet - write a function that prints the alphabet
+ * function
  * Return: void
  */
 void print_alphabet(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar (alphabet);
 	}
