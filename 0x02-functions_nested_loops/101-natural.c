931ce9d66280e89149c8d81ae1cfff5fbe704dfe
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-09-17 06:22:34 -0700

    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print multiples
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n, n2, multiples;
+
+	while (n = 3 || n = 5)
+	{
+		n2 = 1;
+
+		n2++;
+		multiples = (n * n2);
+		if (muliples == 1024)
+		{
+			return (0);
+		}
+	}
+
+}
