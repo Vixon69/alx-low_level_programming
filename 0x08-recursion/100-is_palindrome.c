4fd14caa2912a6ee9cadce5a472ca6826ea8b9df
Vixon69 <99638199+Vixon69@users.noreply.github.com>
2022-10-01 13:54:26 +0100

    Write a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,50 @@
 #include "main.h"
+
 /**
- * is_palindrome - a function that returns 1 if a string is a
- * palindrome and 0 if not.
- * @s: pointer to the string
- * Return: 1 or 0
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
  */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
 int is_palindrome(char *s)
 {
-	if (*s >= 0 && *s <= 9)
-		return (1);
-	else if ((s[0]) == (*s / 10))
-		return (0);
-	return (1);
+	int len = _strlen_recursion(s);
 
+	return (p1(s, len - 1));
 }
