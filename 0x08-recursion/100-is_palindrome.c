7d13af36e03f9701be7c91a1f003ab411cc153bf
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-10-01 05:49:01 -0700

    Write a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 /**
  * is_palindrome - a function that returns 1 if a string is a
  * palindrome and 0 if not.
- * @s - pointer to the string
+ * @s: pointer to the string
  * Return: 1 or 0
  */
 int is_palindrome(char *s)
@@ -11,6 +11,6 @@ int is_palindrome(char *s)
 		return (1);
 	else if ((s[0]) == (*s / 10))
 		return (1);
-	return (0);
+	return (1);
 
 }
