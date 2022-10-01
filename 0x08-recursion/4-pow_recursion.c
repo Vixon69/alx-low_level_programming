dc9d07cd3a6dda296b23ae13a953183e3f31cf5f
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-10-01 04:10:04 -0700

    Write a function that returns the value of x raised to the power of y.

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - a function that returns a value of x raised
+ * to the power of y
+ * @x: value to raised
+ * @y: the power
+ * Return:an int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y--));
+}
