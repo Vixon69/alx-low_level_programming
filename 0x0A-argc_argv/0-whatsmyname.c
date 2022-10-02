ed05c08604e33b708018cf6d97b1d5fef4cadf77
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-10-02 07:09:00 -0700

    Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,12 @@
 
 /**
  * main - a program that prints its name followed by a new line.
- * @argc: argument char
  * @argv: argument vector
  * Return: success
  */
 
-int main(int argc, char *argv[])
+int main(char *argv[])
 {
-	printf("%s\n", argv[argc]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
