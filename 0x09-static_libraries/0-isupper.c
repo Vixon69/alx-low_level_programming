8866f439051c03d7dd11ebdf77ae716b29a4de1c
Vixon69 <99638199+Vixon69@users.noreply.github.com>
2022-10-04 10:53:22 +0100

    update to  0x09-static_libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _isupper - check for uppercase letters
+ *
+ * @c: An ascii character
+ *
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+int _isupper(int c)
+
+{
+	int r;
+	char upper;
+
+	for (upper = 65; upper <= 90; upper++)
+	{
+		if (c >= 0 && c <= 64)
+		{
+			return (0);
+		}
+		else if (c >= 91 && c <= 126)
+			return (0);
+		if (c == upper)
+		{
+			r = 1;
+			break;
+		}
+	}
+	return (r);
+}
