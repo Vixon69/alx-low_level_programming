3585e8fb70dfcd6f6dcf89ea76961a81babcddc6
Vixon69 <99638199+Vixon69@users.noreply.github.com>
2022-10-04 11:00:14 +0100

    Create 0-memset.c

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - Write a function that fills memory with a constant byte.
+ * @s: the memory address
+ * @b: the byte
+ * @n: the number of times
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j != n; j++)
+	{
+		*(s + j) = b;
+	}
+	return (s);
+}
