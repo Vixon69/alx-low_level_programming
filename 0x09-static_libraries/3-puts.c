ded2b507078e882d1dd9d5807a38261b76651795
Vixon69 <99638199+Vixon69@users.noreply.github.com>
2022-10-04 10:55:34 +0100

    Create 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string with a newline
+ * @str: a pointer
+ * return: 0
+ */
+
+void _puts(char *str)
+{
+	int n = 0;
+
+	while ((str[n]) != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+
+}
