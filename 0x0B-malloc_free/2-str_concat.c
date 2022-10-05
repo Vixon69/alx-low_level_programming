867b9f771bb9de9f22d6baacced525b6f86efec0
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-10-05 05:16:31 -0700

    Write a function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,51 +10,24 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, size;
+	int i, j;
 
-	char *ch;
+	char *ch, *ch2;
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	size = i + j;
+	ch = (char *)malloc(sizeof(char) * (i));
+	ch2 = (char *)malloc(sizeof(char) * (j + 1));
 
-	ch = (char *)malloc(sizeof(char) * (size + 1));
 
 	if (ch == NULL)
 		return (NULL);
-	if ((s1[0] != '\0') && (s2[0] != '\0'))
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			*ch = _putchar(s1[i]);
-		}
-		for (i = 0; s2[j] != '\0'; i++)
-		{
-			*ch = _putchar(s2[j]);
-		}
-		return (ch);
-	}		
-
-	if (s1[0] == '\0')
-	{
-		for (i = 0; s2[i] != '\0'; i++)
-		{
-			*ch = _putchar(s2[i]);
-		}
-		return (ch);
-	}
-	if (s2[0] == '\0')
-	{
-		for (j = 0; s1[j] < '\0'; j++)
-		{
-			*ch = _putchar(s1[j]);
-		}
-		return(ch);
-	}
+	
+	if (ch2 == NULL)
+		return (NULL);
 
 	return (NULL);
-
 }
