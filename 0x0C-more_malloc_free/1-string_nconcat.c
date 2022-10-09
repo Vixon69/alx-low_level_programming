a9bc772ae7997ec974650f4d54dc725bbef48584
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-10-09 08:11:22 -0700

    Write a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,11 +30,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 
-	for (i = 0; s1[i] <= '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		f[i] = s1[i];
 	}
-	for (j = i; f[j] < s2[n]; j++)
+	for (j = i; f[j] != s2[n]; j++)
 	{
 		f[j] = s2[j];
 	}
