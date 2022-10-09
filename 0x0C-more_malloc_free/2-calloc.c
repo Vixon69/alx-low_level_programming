7ef7572ae356006e9e6cf171e243e26c0e8d9462
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-10-09 10:25:00 -0700

    Write a function that concatenates all the arguments of your program.

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _calloc - Write a function that allocates memory for an array, using malloc.
+ * @nmemb:This is the number of elements to be allocated.
+ * @size:This is the size of elements.
+ * Return:NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	
+}
