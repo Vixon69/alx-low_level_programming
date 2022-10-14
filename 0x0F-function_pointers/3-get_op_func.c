2c01d5c0390f5b0640cd8f024fe66d092b6bb7bf
vixon69 <19-30gc024@students.unilorin.edu.ng>
2022-10-14 04:37:34 -0700

    Write a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "calc.h"
+#include <string.h>
+
+/**
+ * get_op_func - function pointer taht selects the current function to perform
+ * he operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the 
+ * operator given as a parameter
+ */
+
+int(*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
